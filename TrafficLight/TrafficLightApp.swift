6568a82e1cab7e8d90d2a83373ac30b9eeee3f1f
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-08-06 23:56:33 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Alexandr Artemov (Mac Mini) on 06.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
